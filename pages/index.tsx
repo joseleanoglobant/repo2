1d09e62c5dda5acc36dde672c5274d861687bc27
jose <jose.leano@globant.com>
2021-10-20 14:08:46 -0500

    Cambio de repo
    Se crea nuevo repo con update en la raiz del directorio

---
+++ pages/index.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import bootstrap from 'bootstrap'
+import styles from '../styles/Home.module.css'
+import { fetchEntries } from '../utils/contentfulPosts'
+import Editorial from '../src/components/editorial'
+
+
+export default function Home({ editorial }) {
+  return (
+    <div className="container">
+      <Head>
+        <title>Happy Birthday!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+
+        <div className="posts">
+
+          {editorial.map((p) => {
+            return <Editorial title={p.title} category={p.category} date={p.date} image={p.editorialImage.fields} author={p.author} featured={p.featured} order={p.order}/>
+          })}
+
+        </div>
+      </main>
+
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const res = await fetchEntries()
+  const editorial = await res.map((p) => {
+    return p.fields
+  })
+
+  console.log(editorial)
+
+  return {
+    props: {
+      editorial,
+    },
+  }
+}
